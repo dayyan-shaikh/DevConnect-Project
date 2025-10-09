594e01b9cce8d3cebf4b5de3372f034ae60b7edb
dayyan-edmission <dayayan.shaikh@edmissioncrm.com>
2025-10-09 19:11:57 +0530

    Update server port configuration to use environment variable or default to 3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     credentials: true, // if you want cookies or auth headers
   });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
